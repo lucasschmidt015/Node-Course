d8b635e8f775bcca3fa2e8c7e1decfb41ffb259b
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-08-28 21:32:58 -0300

    Implemted the edit and delete option.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,20 +1,70 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { 
+    res.render('admin/edit-product', { 
         pageTitle: 'Add Product',
-        path: '/admin/add-product',  
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true,                                
+        path: '/admin/add-product',                 
+        editing: false          
     });
 }
 
 exports.postAddProduct = (req, res) => {
     const body = req.body;
-    const product = new Product(body.title, body.imageUrl, body.description, body.price);
+    const product = new Product(null, body.title, body.imageUrl, body.description, body.price);
     product.save();
     res.redirect('/');
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const prodId = req.params.productId;
+
+    Product.findById(prodId, (product) => {
+
+        if (!product) {
+            return res.redirect('/');
+        }
+
+        res.render('admin/edit-product', { 
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',        
+            editing: editMode,              
+            product: product     
+        });
+    })    
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatePrice = req.body.price;
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatePrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    const productId = req.body.productId;
+    console.log('My product id is: ', productId);
+
+    if (!productId) {
+        return res.redirect('/admin/products');
+    }
+
+    Product.deleteById(productId, (successfull) => {
+        if (successfull) {
+            console.log('Deleted');
+        } else {
+            console.log('undeleted');
+        }
+        res.redirect('/admin/products');
+    })
 }
 
 exports.getProducts = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -21,7 +21,8 @@ const getProductsFromFile = (cb) => {
 
 //https://cdn.pixabay.com/photo/2016/03/31/20/51/book-1296045_960_720.png
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -29,12 +30,27 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
+
+        
+
         getProductsFromFile((products) => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
-            })
+
+            if (this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+                const updatedProduct = [...products];
+                updatedProduct[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+                    console.log(err);
+                })
+            }
+            else {
+                this.id = Math.random().toString();
+
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    console.log(err);
+                })
+            }
         })
     }
 
@@ -46,6 +62,22 @@ module.exports = class Product {
         getProductsFromFile((products) => {
             const product = products.find(p => p.id === id);
             cb(product);
+        })
+    }
+
+    static deleteById(id, cb) {
+        getProductsFromFile(products => {
+            const productIndex = products.findIndex(prod => prod.id === id);
+            const deletedArray = [...products];
+            deletedArray.splice(productIndex, 1);
+            fs.writeFile(p, JSON.stringify(deletedArray), err => {
+                if (err) {
+                    cb(false);
+                }
+                else {
+                    cb(true);
+                }
+            })
         })
     }
 }
