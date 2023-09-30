dcd36a832315bee02b96e3a9847b015b703eaac3
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-09-30 11:03:00 -0300

    Configuring the MongoDB features.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
+const mongoDb = require('mongodb');
 
+const ObjectId = mongoDb.ObjectId;
 //you have to replace all occurrences of findById with findByPk()
 
 exports.getAddProduct = (req, res, next) => {
@@ -15,12 +17,8 @@ exports.postAddProduct = (req, res) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    req.user.createProduct({
-        title: title,
-        price: price,
-        imageUrl: imageUrl,
-        description: description,
-    })
+    const product = new Product(title, price, description, imageUrl)
+    product.save()
     .then(result => {
         console.log('Created product');
         res.redirect('/admin/products');
@@ -39,10 +37,8 @@ exports.getEditProduct = (req, res, next) => {
 
     const prodId = req.params.productId;
 
-    req.user.getProducts({where: {id: prodId}})
-    //Product.findByPk(prodId)
-    .then(products => {
-        const product = products[0];
+    Product.findById(prodId)
+    .then(product => {
 
         if (!product) {
             return res.redirect('/');
@@ -67,38 +63,31 @@ exports.postEditProduct = (req, res, next) => {
     const updatedDescription = req.body.description;
     const updatePrice = req.body.price;
     
-    Product.findByPk(prodId)
-    .then(product => {
-        product.title = updatedTitle;
-        product.price = updatePrice;
-        product.imageUrl = updatedImageUrl;
-        product.description = updatedDescription;
-        return product.save();
-    })
+    const updatedProduct = new Product(updatedTitle, updatePrice, updatedDescription, updatedImageUrl, new ObjectId(prodId));
+    updatedProduct.save()
     .then(result => {
         res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const productId = req.body.productId;
+// exports.postDeleteProduct = (req, res, next) => {
+//     const productId = req.body.productId;
 
-    Product.findByPk(productId)
-    .then(product => { 
-        return product.destroy();
-    })
-    .then(result => {
-        console.log('Destroyed product');
-        res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
-}
+//     Product.findByPk(productId)
+//     .then(product => { 
+//         return product.destroy();
+//     })
+//     .then(result => {
+//         console.log('Destroyed product');
+//         res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err));
+// }
 
 exports.getProducts = (req, res, next) => {
 
-    // Product.findAll()
-    req.user.getProducts()
+    Product.fetchAll()
     .then(products => {
         res.render('admin/products', { 
             prods: products,
--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '123456', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://lucasschmidt015:eHhMQePUAzCeWbzU@cluster0.nq2w9zx.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(Client => {
+        console.log('Connected!');
+        _db = Client.db();
+        callback();
+    })
+    .catch(err => {
+        console.log(err);
+        throw err;
+    })
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
