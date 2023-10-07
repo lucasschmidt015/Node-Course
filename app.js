82acc30b5d1c27fa76992d89dd17c77e1432caee
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-10-06 21:06:36 -0300

    Started tha authentication module.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -10,24 +12,41 @@ const notFoundController = require('./controllers/error');
 
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://lucasschmidt015:eHhMQePUAzCeWbzU@cluster0.nq2w9zx.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({ 
+        secret: 'your-secret-key',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('6519ca762896bbba96622e49')
-    .then(user => {
+
+    if (!req.session.user) {
+      return next();
+    }
+    User.findById(req.session.user._id)
+      .then(user => {
         req.user = user;
         next();
-    })
-    .catch(err => {
-        console.log(err);
-    });
-})
+      })
+      .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
@@ -35,21 +54,8 @@ app.use(authRouter);
 
 app.use(notFoundController.get404);
 
-mongoose.connect('mongodb+srv://lucasschmidt015:eHhMQePUAzCeWbzU@cluster0.nq2w9zx.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
 .then(result => {
-    User.findOne().then(user => {
-        if (!user) {
-            const newUser = new User({ 
-                name: 'Lucas',
-                email: 'teste@teste.com',
-                cart: {
-                    items: []
-                }
-             });
-             newUser.save();
-        }
-    })
-    
     app.listen(4000);
 })
 .catch(err => console.log(err));
